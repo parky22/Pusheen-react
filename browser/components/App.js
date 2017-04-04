be2f30826d0f689e5b13f9ec44857e364dfc8ac0
Yoo-Nah Park <ypark2270@gmail.com>
2017-04-04 16:58:01 -0400

    update package.json and redesign App.j

--- browser/components/App.js
+++ browser/components/App.js
@@ -13,14 +13,20 @@ class App extends React.Component {
   render () {
     return (
       <div>
+        <h1 style={{textAlign: 'center'}}> Welcome to React with Pusheen! </h1>
+        <hr />
         <div>
+          <h3> 1. Change me to a "All the Pusheens" link </h3>
           {
             /*
             Show a link called "All the Pusheens" that will present a list of all the pusheens when clicked
             */
+
           }
         </div>
+        <hr />
         <div>
+          <h3> 2. Change this area so that it can switch between a list of all Pusheens and a single Pusheen </h3>
           {
             /*
             This area should show 1) list of all the pusheens  or 2) one pusheens
